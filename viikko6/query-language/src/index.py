d52853aba02f2dd821f73ad2d917955f14524164
faintworks <iiro.e.laitinen@helsinki.fi>
2025-12-09 20:31:31 +0200

    Add query functionality

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,21 +1,28 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
 
-    for player in stats.matches(matcher):
+    matcher_nyr_not_2_goals = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
+    for player in sorted(stats.matches(matcher_nyr_not_2_goals), key=lambda p: p.name):
         print(player)
 
+    matcher_nyr_fewer_2_goals = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+    for player in sorted(stats.matches(matcher_nyr_fewer_2_goals), key=lambda p: p.name):
+        print(player)
 
 if __name__ == "__main__":
     main()
