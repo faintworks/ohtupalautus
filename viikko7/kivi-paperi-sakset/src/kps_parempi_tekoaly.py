59ddcff9f97dbf210ac74bcfb7effe9a444ad9a1
faintworks <iiro.e.laitinen@helsinki.fi>
2025-12-21 15:25:27 +0200

    Refactor game code

--- viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -1,29 +1,5 @@
-from tuomari import Tuomari
-from tekoaly_parannettu import TekoalyParannettu
+from kps_tekoaly import KPSTekoaly
 
-
-class KPSParempiTekoaly:
-    def pelaa(self):
-        tuomari = Tuomari()
-        tekoaly = TekoalyParannettu(10)
-
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
-
-        print(f"Tietokone valitsi: {tokan_siirto}")
-
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
-
-            print(f"Tietokone valitsi: {tokan_siirto}")
-            tekoaly.aseta_siirto(ekan_siirto)
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+class KPSParempiTekoaly(KPSTekoaly):
+    def __init__(self, tekoaly):
+        super().__init__(tekoaly)
